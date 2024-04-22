db1de336a5e705cc554f5ccab3ac31bfdddca3f0
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-22 11:26:05 +0300

    .

--- src/main/java/com/tugalsan/api/cast/server/TS_CastUtils.java
+++ src/main/java/com/tugalsan/api/cast/server/TS_CastUtils.java
@@ -1,8 +1,8 @@
 package com.tugalsan.api.cast.server;
 
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import java.nio.file.Path;
-import java.util.Optional;
 
 public class TS_CastUtils {
 
@@ -20,7 +20,7 @@ public class TS_CastUtils {
         return String.format("%." + precision + "f", value);//GWT DOES NOT LIKE U
     }
 
-    public static Optional<Path> toPath(CharSequence path) {
-        return TGS_UnSafe.call(() -> Optional.of(Path.of(path.toString())), e -> Optional.empty());
+    public static TGS_UnionExcuse<Path> toPath(CharSequence path) {
+        return TGS_UnSafe.call(() -> TGS_UnionExcuse.of(Path.of(path.toString())), e -> TGS_UnionExcuse.ofExcuse(e));
     }
 }
