7b642625c95bcd66b87351b70d772e1e4f99686d
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:09:49 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.tugalsan.api.cast {
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.string;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     exports com.tugalsan.api.cast.client;
     exports com.tugalsan.api.cast.server;
 }
